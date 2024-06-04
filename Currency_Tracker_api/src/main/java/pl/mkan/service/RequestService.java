8890c4924fb564019ed74b0ade42858d33d7fe86
maciej-MKan <kanabaj.maciej@gmail.com>
2024-06-05 00:57:45 +0200

    backend-add_requestService_implementation

--- Currency_Tracker_api/src/main/java/pl/mkan/service/RequestService.java
+++ Currency_Tracker_api/src/main/java/pl/mkan/service/RequestService.java
@@ -1,19 +1,33 @@
 package pl.mkan.service;
 
-import org.springframework.http.ResponseEntity;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.mkan.controller.dto.CurrencyRequestDTO;
 import pl.mkan.controller.dto.RequestDTO;
+import pl.mkan.persistance.database.model.CurrencyRequestEntity;
+import pl.mkan.persistance.database.model.mapper.CurrencyRequestMapper;
+import pl.mkan.persistance.database.repository.CurrencyRequestRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@AllArgsConstructor
 @Service
 public class RequestService {
-    public void saveRequest(CurrencyRequestDTO request) {
+    private CurrencyRequestRepository repository;
+    private CurrencyRequestMapper mapper;
 
+    public void saveRequest(CurrencyRequestDTO request, double value) {
+        repository.save(CurrencyRequestEntity.builder()
+                .name(request.name())
+                .currency(request.currency())
+                .date(LocalDateTime.now())
+                .mid(value)
+                .build()
+        );
     }
 
-    public ResponseEntity<List<RequestDTO>> getAll() {
-        return null;
+    public List<RequestDTO> getAll() {
+        return repository.findAll().stream().map(mapper::toDTO).toList();
     }
 }
