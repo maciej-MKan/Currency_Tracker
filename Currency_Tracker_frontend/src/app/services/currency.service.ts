83d605d52faf2a9979e7c80b34cb825cab5a683d
MaciejKan <101216341+maciej-MKan@users.noreply.github.com>
2024-06-18 00:13:35 +0200

    Update env (#8)
    * global-add_logo
    
    * global-readme_update
    
    * backend-fix_time_zone_problem
    
    * global-update_env

--- Currency_Tracker_frontend/src/app/services/currency.service.ts
+++ Currency_Tracker_frontend/src/app/services/currency.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../enviroments/enviroment';
+import { environment } from '../../../environments/environment';
 import { Request } from '../models/request.model';
 
 @Injectable({
--- Currency_Tracker_frontend/src/assets/scripts/set-env.js
+++ Currency_Tracker_frontend/src/assets/scripts/set-env.js
@@ -1,7 +1,7 @@
-const { writeFileSync } = require('fs');
-const { resolve } = require('path');
+const {writeFileSync} = require('fs');
+const {resolve} = require('path');
 
-const targetPath = resolve(__dirname, '../../environments/environment.ts');
+const targetPath = resolve(__dirname, '../../../environments/environment.ts');
 const envConfigFile = `export const environment = {
   production: false,
   apiUrl: '${process.env.API_URL}'
@@ -10,7 +10,7 @@ const envConfigFile = `export const environment = {
 
 writeFileSync(targetPath, envConfigFile, 'utf8');
 
-const targetPathProd = resolve(__dirname, '../../environments/environment.prod.ts');
+const targetPathProd = resolve(__dirname, '../../../environments/environment.prod.ts');
 const envConfigFileProd = `export const environment = {
   production: true,
   apiUrl: '${process.env.API_URL}'
